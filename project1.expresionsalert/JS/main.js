884f5ada183376be4533fd89bf228d10e50ca53f
andreazambranor <101218502+andreazambranor@users.noreply.github.com>
2022-08-08 23:30:14 -0400

    commited all changes

---
+++ project1.expresionsalert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("this is how yo put an alert box in JavaScript"); //creating a pop up window with a message
+var assign="THis is how you assign a variable"; //assigning value to a variable 
+document.write(assign); //instructing to print the variable
+document.write("this is how"+"you concatenate"+"multiple strings"); //printing concatenated strings 
+document.write("this is how you get your pc to write characters like \" \' \\ with JavaScript"); //printing strings with special characters
+var characters="escaping", concatenate="plus operator", colors="string name dot fontcolor", variables="assign multiple variables with comas"; //assigning multiple values in a statement
+var variables= variables.fontcolor("blue"); //assigning a font color to a variable 
+document.write (variables); //printing a variable 
+document.write (10+15); //print the result of the expression
---
+++ project2.functions/java/main.js
@@ -0,0 +1,10 @@
+function Red_stinger() {
+    var str="this is a red car";
+    var result=str.fontcolor("red");
+    document.getElementById("red_car") .innerHTML=result;
+}
+function myFunction() {
+    var sentence="hello!";
+    sentence += "today is monday";
+    document.getElementById("Greetings") .innerHTML = sentence
+}
