283239c49c856cb9b1762e23707fc5dd8ef8124f
andreazambranor <101218502+andreazambranor@users.noreply.github.com>
2022-09-15 14:34:59 -0400

    submission assignment number 6
    project 6 ternary operators constructors

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -10,7 +10,7 @@ function vehicle (make, model, year, color) {
     this.vehicle_model=model;
     this.vehicle_year=year;
     this.vehicle_color=color;
-} //Determining the object's properties
+} //Object constructor challenge 36
 
 var jack=new vehicle("dodge", "viper", 2020, "red");//New instance
 var emily=new vehicle("jeep", "trail hawk", 2019, "white and black");//new instance
@@ -25,11 +25,21 @@ function player(name, surname, age, team) {
     this.player_surname=surname;
     this.player_age=age;
     this.player_team=team;
-}
+}//object constructor challenge 37
 
 var player12=new player("Aaron", "Rodgers", 38, "Green Bay");
 var Player9=new player("Christian", "Watson", 23, "Green Bay");
 var player33=new player("Aaron", "JOnes", 27, "Green Bay");
 function player_function() {
     getElementById(new_and_this).innerHTML="player 12 is "+player12.player_name+player12.player_surname+" and is "+player12.player_age+" years old.";
+} // function to return player
+
+function mynestedfunction() {
+    document.getElementById("nested_function").innerHTML=nested();
+    function nested() {
+        var x=24;
+        function addten() {x+=10;}
+        addten();
+        return x;
+    }
 }
